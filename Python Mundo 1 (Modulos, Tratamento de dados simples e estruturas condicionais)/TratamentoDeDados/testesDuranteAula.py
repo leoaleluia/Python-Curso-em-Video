735d560c36e929afa957e84452b851deb8ba87d4
leoaleluia <aleluiasp17@hotmail.com>
2025-04-01 14:37:37 -0300

    Update testesDuranteAula.py

--- Python Mundo 1 (Modulos, Tratamento de dados simples e estruturas condicionais)/TratamentoDeDados/testesDuranteAula.py
+++ Python Mundo 1 (Modulos, Tratamento de dados simples e estruturas condicionais)/TratamentoDeDados/testesDuranteAula.py
@@ -4,5 +4,11 @@ aluno3 = int(input("Digite a nota do terceiro aluno"))
 media = (aluno1 + aluno2 + aluno3) / 3
 print(f"a média dos alunos é {media}")"""
 
-nome = input("Digite seu nome")
-print('bem vindo {:>20}'.format(nome)) #alinha 20 casas para a direita (< alinha para a esquerda) (^alinha para o centro)
+"""nome = input("Digite seu nome")
+print('bem vindo {:>20}'.format(nome)) #alinha 20 casas para a direita (< alinha para a esquerda) (^alinha para o centro)"""
+
+from math import sqrt
+numero = int(input("Digite um número para saber a raiz"))
+raiz = sqrt(numero).__trunc__()
+print(f'a raiz do número {numero} é {raiz}')
+      
