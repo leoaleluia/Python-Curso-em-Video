e20813b782c253dfb843c948edefc9b2bb198f40
leoaleluia <aleluiasp17@hotmail.com>
2025-04-24 16:48:32 -0300

    Update Valores_Unicos_em_Listas.py

--- Python Mundo 3 (Tuplas, Listas, Dicionários e Tratamento de Erros/Listas/Valores_Unicos_em_Listas.py
+++ Python Mundo 3 (Tuplas, Listas, Dicionários e Tratamento de Erros/Listas/Valores_Unicos_em_Listas.py
@@ -1,14 +1,18 @@
 numeros = list()
 while True:
     num = int(input("Digite um número: "))
+    
     while num in numeros:
         num = int(input(f"Digite um número que não tenha sido inserido na lista, confira: {numeros}"))
+
     numeros.append(num) #após verificar o num digitado, adiciona a variável num à lista
     resposta = input("Deseja continuar? [S/N]")
+
     while resposta.upper().strip()[0] not in 'SN':
         resposta = input("Digite uma opção válida. [S/N]").upper().strip()
+
     if resposta[0].upper().strip() == 'N':
         break
 
-numeros.sort()
-print(numeros) #Oganiza em ordem crescente
+numeros.sort()#Oganiza em ordem crescente
+print(numeros) 
