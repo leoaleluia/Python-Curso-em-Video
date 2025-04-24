f6c1d8a0eb2540864b91e096bde945b37c1015c2
leoaleluia <aleluiasp17@hotmail.com>
2025-04-24 15:29:00 -0300

    aperfeiçoamento

--- Python Mundo 3 (Tuplas, Listas, Dicionários e Tratamento de Erros/Tuplas em Python/Análise_de_Dados_em_tuplas.py
+++ Python Mundo 3 (Tuplas, Listas, Dicionários e Tratamento de Erros/Tuplas em Python/Análise_de_Dados_em_tuplas.py
@@ -3,4 +3,8 @@ numeros = (int(input("Digite um númeo")), int(input("Digite o segundo número")
 print(f"o número 9 apaececeu {numeros.count(9)} vez")
 for c in numeros:
     if c % 2 == 0:
-        print(f"O número {c} é par")
+        print(f"O número {c} é par", end='')
+if 3 in numeros:
+    print(f"o número 3 esta na posição {numeros.index(3)}")
+else:
+    print("o número 3 não foi digitado em nenhuma tupla")
--- Python Mundo 3 (Tuplas, Listas, Dicionários e Tratamento de Erros/Tuplas em Python/Tabela_de_Preco.py
+++ Python Mundo 3 (Tuplas, Listas, Dicionários e Tratamento de Erros/Tuplas em Python/Tabela_de_Preco.py
@@ -1,6 +1,9 @@
-tabelaProduto = ('Lápis', 'Borracha', 'Caderdo', 'Estojo','Transferidor', 'Compasso', 'Mochila',  'Canetas', 'Livro',)
-tabelaPreco = ( 1.75, 2.00, 15.00, 25.00, 4.20, 9.99, 128.32, 22.20, 34.98)
+tabelaProduto = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderdo', 15,'Estojo', 25, 'Transferidor', 4.20, 'Compasso',  9.99, 'Mochila', 128, 'Canetas', 22.20, 'Livro', 34.98) 
 
-for c in range(len(tabelaProduto)):
-    print(f"{tabelaProduto[c]}..........R${tabelaPreco[c]}")
+print(f"{"Lista De preço":^20}") #ao usar essa fString, podemos colocar uma string em uma chave e manipular a mesma
+for pos in range(len(tabelaProduto)): #dessa maneira o pos assume o valor de indice puro, ou seja, numero inteiro
+    if pos % 2 == 0:
+        print(f"{tabelaProduto[pos]:.<20}", end='')#alinhou a esqueca com 20 espaços
+    else:
+        print(f"R${tabelaProduto[pos]:.2f}")
 
